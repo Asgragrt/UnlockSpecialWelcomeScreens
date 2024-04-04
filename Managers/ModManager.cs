d07d78b2e9bb3bdb42525ac5b4acce79c8a362db
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-03 21:17:04 -0600

    :art: Small changes

---
+++ Managers/ModManager.cs
@@ -0,0 +1,10 @@
+﻿namespace UnlockSpecialWelcomeScreens.Managers;
+
+internal static class ModManager
+{
+    internal static readonly List<int> WelcomeIndexes =
+    [
+        22, // ?
+        23, // Marija
+    ];
+}
--- Patches/DBPatch.cs
+++ Patches/DBPatch.cs
@@ -1,13 +1,16 @@
 ﻿using HarmonyLib;
 using Il2CppAssets.Scripts.Database;
+using MelonLoader;
+using UnlockSpecialWelcomeScreens.Managers;
 
 namespace UnlockSpecialWelcomeScreens.Patches;
 
 [HarmonyPatch(typeof(DBConfigWelcome), nameof(DBConfigWelcome.GetWelcomeInfoByIndex))]
-internal static class WelcomeInfoPatch
+internal static class DBPatch
 {
-    internal static void Postfix(WelcomeInfo __result)
+    internal static void Postfix(int index, WelcomeInfo __result)
     {
+        if (!ModManager.WelcomeIndexes.Contains(index)) return;
         __result.exchange = false;
     }
 }
