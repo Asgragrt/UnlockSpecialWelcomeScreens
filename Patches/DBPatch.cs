28376a6e1f5682125c9e0da82f3c850c79dbf1ed
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-11 16:41:15 -0600

    :sparkles: Unlock special loading screens

--- Patches/DBPatch.cs
+++ Patches/DBPatch.cs
@@ -4,12 +4,27 @@ using UnlockSpecialWelcomeScreens.Managers;
 
 namespace UnlockSpecialWelcomeScreens.Patches;
 
+//--------------------------------------------------------------------+
+// Welcome screens
+//--------------------------------------------------------------------+
 [HarmonyPatch(typeof(DBConfigWelcome), nameof(DBConfigWelcome.GetWelcomeInfoByIndex))]
 internal static class DBPatch
 {
     internal static void Postfix(int index, WelcomeInfo __result)
     {
         if (!ModManager.WelcomeIndexes.Contains(index)) return;
+        __result.exchange = false;
+    }
+}
+
+//--------------------------------------------------------------------+
+// Loading screens
+//--------------------------------------------------------------------+
+[HarmonyPatch(typeof(DBConfigLoading), nameof(DBConfigLoading.GetLoadingInfoByIndex))]
+internal static class DBPatch2
+{
+    internal static void Postfix(LoadingInfo __result)
+    {
         __result.exchange = false;
     }
 }
