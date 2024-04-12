fce2d9430a56337aa6ee47e9cd07e4915a567ee8
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-11 19:53:58 -0600

    :art: Merge patches inside the same class

--- Patches/DBPatch.cs
+++ Patches/DBPatch.cs
@@ -6,27 +6,28 @@ namespace UnlockSpecialWelcomeScreens.Patches;
 
 using static ModManager;
 
-//--------------------------------------------------------------------+
-// Welcome screens
-//--------------------------------------------------------------------+
-[HarmonyPatch(typeof(DBConfigWelcome), nameof(DBConfigWelcome.GetWelcomeInfoByIndex))]
+[HarmonyPatch]
 internal static class DBPatch
 {
-    internal static void Postfix(int index, WelcomeInfo __result)
+    //--------------------------------------------------------------------+
+    // Welcome screens
+    //--------------------------------------------------------------------+
+    [HarmonyPatch(typeof(DBConfigWelcome), nameof(DBConfigWelcome.GetWelcomeInfoByIndex))]
+    [HarmonyPostfix]
+    internal static void WelcomePostfix(int index, WelcomeInfo __result)
     {
         if (!__result.exchange) return;
-        
+
         WelcomeExchangeIndexes.Add(index);
     }
-}
 
-//--------------------------------------------------------------------+
-// Loading screens
-//--------------------------------------------------------------------+
-[HarmonyPatch(typeof(DBConfigLoading), nameof(DBConfigLoading.GetLoadingInfoByIndex))]
-internal static class DBPatch2
-{
-    internal static void Postfix(LoadingInfo __result)
+    //--------------------------------------------------------------------+
+    // Loading screens
+    //--------------------------------------------------------------------+
+
+    [HarmonyPatch(typeof(DBConfigLoading), nameof(DBConfigLoading.GetLoadingInfoByIndex))]
+    [HarmonyPostfix]
+    internal static void LoadingPostfix(LoadingInfo __result)
     {
         __result.exchange = false;
     }
