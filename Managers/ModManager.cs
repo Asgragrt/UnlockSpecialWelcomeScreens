9f464de3d2b76983fcddadddee9f23bf57176e8d
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-11 20:15:31 -0600

    :art: Code refactor

--- Managers/ModManager.cs
+++ Managers/ModManager.cs
@@ -1,16 +1,42 @@
-﻿using Il2CppAssets.Scripts.PeroTools.Nice.Interface;
+﻿using Il2CppAssets.Scripts.GameCore.Managers;
+using Il2CppAssets.Scripts.PeroTools.Nice.Interface;
 
 namespace UnlockSpecialWelcomeScreens.Managers;
 
 internal static class ModManager
 {
     internal static readonly HashSet<int> WelcomeExchangeIndexes = [];
-    
+
     internal static readonly HashSet<int> LoadingExchangeIndexes = [];
 
-    internal static bool IsWelcome(IData data) => string.Equals(data["type"].GetResult<string>(), "welcome");
-    
-    internal static bool IsLoading(IData data) => string.Equals(data["type"].GetResult<string>(), "loading");
+    private static bool IsWelcome(IData data) => string.Equals(data["type"].GetResult<string>(), "welcome");
 
-    internal static bool CheckDataIndex(IData data, int index) => data["index"].GetResult<int>() == index;
+    private static bool IsLoading(IData data) => string.Equals(data["type"].GetResult<string>(), "loading");
+
+    private static bool CheckDataIndex(IData data, int index) => data["index"].GetResult<int>() == index;
+
+    private static void UnlockExchange(ItemManager itemManager, Func<IData, bool> filter, HashSet<int> exchangeIndexes,
+        string type)
+    {
+        var unlockedItems = itemManager.items.FindAll(filter);
+
+        exchangeIndexes.RemoveWhere(
+            idx => unlockedItems.Exists(
+                (Func<IData, bool>)(data => CheckDataIndex(data, idx))
+            )
+        );
+
+        foreach (var lockedExchangeIndex in exchangeIndexes)
+            itemManager.AddItem(type, lockedExchangeIndex, 5);
+    }
+
+    internal static void UnlockExchangeWelcome(ItemManager itemManager)
+    {
+        UnlockExchange(itemManager, IsWelcome, WelcomeExchangeIndexes, "welcome");
+    }
+
+    internal static void UnlockExchangeLoading(ItemManager itemManager)
+    {
+        UnlockExchange(itemManager, IsLoading, LoadingExchangeIndexes, "loading");
+    }
 }
--- Patches/ItemManagerPatch.cs
+++ Patches/ItemManagerPatch.cs
@@ -1,7 +1,5 @@
 ﻿using HarmonyLib;
-using Il2CppAssets.Scripts.Database;
 using Il2CppAssets.Scripts.GameCore.Managers;
-using Il2CppAssets.Scripts.PeroTools.Nice.Interface;
 using UnlockSpecialWelcomeScreens.Managers;
 
 namespace UnlockSpecialWelcomeScreens.Patches;
@@ -13,34 +11,7 @@ internal static class ItemManagerPatch
 {
     internal static void Postfix(ItemManager __instance)
     {
-        var items = DataHelper.items;
-
-        //--------------------------------------------------------------------+
-        // Welcome screens
-        //--------------------------------------------------------------------+
-        var unlockedWelcomeList = items.FindAll((Func<IData, bool>)IsWelcome);
-
-        WelcomeExchangeIndexes.RemoveWhere(
-            idx => unlockedWelcomeList.Exists(
-                (Func<IData, bool>)(data => CheckDataIndex(data, idx))
-            )
-        );
-
-        foreach (var lockedExchangeWelcomeIndex in WelcomeExchangeIndexes)
-            __instance.AddItem("welcome", lockedExchangeWelcomeIndex, 5);
-
-        //--------------------------------------------------------------------+
-        // Welcome screens
-        //--------------------------------------------------------------------+
-        var unlockedLoadingList = items.FindAll((Func<IData, bool>)IsLoading);
-
-        LoadingExchangeIndexes.RemoveWhere(
-            idx => unlockedLoadingList.Exists(
-                (Func<IData, bool>)(data => CheckDataIndex(data, idx))
-            )
-        );
-
-        foreach (var lockedExchangeLoadingIndex in LoadingExchangeIndexes)
-            __instance.AddItem("loading", lockedExchangeLoadingIndex, 5);
+        UnlockExchangeWelcome(__instance);
+        UnlockExchangeLoading(__instance);
     }
 }
